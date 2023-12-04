15fff6ebc6f2aaa6e4aa8b8b11de9704b473750f
designernagesh <designer.nagesh@gmail.com>
2023-12-04 23:20:43 +0530

    modified the styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+const url = "https://api.quotable.io/random";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState("");
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setQuote(data);
+    } catch (error) {
+      console.error("Error fetching joke:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch a Quote when the component mounts
+    fetchQuote();
+  }, []);
+
+  const getQuote = () => {
+    fetchQuote();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="container">
+        <h2 className="title">Random Quote Generator</h2>
+        <p className="sub-title">
+          Click the button to get the Random Quote! 😊
         </p>
+
+        <span className="emoji">🎯</span> 
+        <p className={`quote ${quote ? "fade" : ""}`}>
+          {quote.content}
+          <span> - {quote.author}</span>
+        </p>
+        <button onClick={getQuote}>Get Random Quote</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
